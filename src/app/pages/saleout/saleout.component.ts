10536e42139d6e3487861d3ba16e7abd3da81c0f
Mabdulah <Mabdulah596@hotmail.com>
2019-01-26 22:54:38 +0500

    update

--- src/app/pages/saleout/saleout.component.ts
+++ src/app/pages/saleout/saleout.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../../services/api/api.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-saleout',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./saleout.component.css']
 })
 export class SaleoutComponent implements OnInit {
-
-  constructor() { }
+prd;
+  constructor(private api:ApiService,private router:Router) { }
 
   ngOnInit() {
+    this.showProducts();
+  }
+  showProducts() {
+    this.api.getProducts().pipe(map(list => list.map(item => {
+      let data = item.payload.doc.data();
+      let id = item.payload.doc.id;
+      return { id, ...data };
+    }))).subscribe(res => {
+      this.prd = res;
+      console.log(res);
+    })
   }
 
 }
