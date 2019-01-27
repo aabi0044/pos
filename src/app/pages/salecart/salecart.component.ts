81bcf03a5d50c0062cc67ce6c66cd63b7a6a3f77
Mabdulah <Mabdulah596@hotmail.com>
2019-01-27 14:25:19 +0500

    update

--- src/app/pages/salecart/salecart.component.ts
+++ src/app/pages/salecart/salecart.component.ts
@@ -12,7 +12,42 @@ export class SalecartComponent implements OnInit {
   ngOnInit() {
   }
   checkoutSaleCart(){
+
+  }
+  checkout() {
+
+
+    let len = this.api.cart.length;
     
+
+    for (let i = 0; i < len; i++) {
+      let res = this.api.cart[i];
+      let id = res.prdid;
+
+      let quantity = res.quantity;
+      this.api.getProduct(id).pipe(first()).toPromise().then(respo => {
+        this.amad = respo;
+
+        console.log(respo);
+
+        this.actualquantity = Number(this.amad.quantity);
+
+        this.leftquantity = this.actualquantity + quantity;
+        let data = {
+          quantity: this.leftquantity,
+
+        }
+        this.api.updateProduct(id, data).then(resp => {
+          console.log('updtaed');
+        })
+
+     
+      })
+
+
+
+    }
+    console.log("loop End");
   }
 
 }
