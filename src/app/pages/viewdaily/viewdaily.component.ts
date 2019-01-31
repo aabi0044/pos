56ec0ac2e6fd5d9adc142c205d80e450e3a89917
Mabdulah <Mabdulah596@hotmail.com>
2019-02-01 02:56:07 +0500

    updates

--- src/app/pages/viewdaily/viewdaily.component.ts
+++ src/app/pages/viewdaily/viewdaily.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import{ApiService} from '../../services/api/api.service';
 
 @Component({
   selector: 'app-viewdaily',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewdailyComponent implements OnInit {
 
-  constructor() { }
+  constructor(private api:ApiService) { }
 
   ngOnInit() {
+  }
+  viewdailybills(){
+    
   }
 
 }
